7f3819f72f2b286de1da79109072b1cea922b3c5
Niklas <Niklas@192.168.178.44>
2017-03-26 18:44:45 +0200

    mhm

--- src/image_mirroring/image.java
+++ src/image_mirroring/image.java
@@ -16,24 +16,36 @@ public class image {
 					{7,8,9},
 					{10,11,12},
 				};
-		mirroring(test);
+		//mirroring(test);
 		
+		System.out.println(loadImage(""));
 	}
 	
-	public static void loadImage(String filename){
+	public static BufferedImage loadImage(String filename){
 		BufferedImage img = null;
 		try {
-		    img = ImageIO.read(new File(filename));
+		    //img = ImageIO.read(new File(filename));
+		    //img = ImageIO.read((getClass().getResourceAsStream(filename)));
 		} catch (IOException e) {
+			System.out.println("cant load image");
+		}
+		return img;
+	}
+	
+	public static void arrayMirroing(int a[]){
+		int h;
+		for (int i = 1; i < a.length; i++) {
+			h = a[i-1];
+			a[i-1] = a[a.length-i];
+			a[a.length-i] = h;
 		}
 	}
 	
 	public static void mirroring(int[][] a){
 		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
-			for (int j = 0; j < a[i].length; j++) {
-				System.out.print(Arrays.toString(a[i][j]));
-			}
+			//System.out.println(Arrays.toString(a[i]));
+			arrayMirroing(a[i]);
+			System.out.println(Arrays.toString(a[i]));			
 		}
 	}
 }
